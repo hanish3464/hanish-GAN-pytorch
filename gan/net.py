f87d9cd847ddeaeb3345368bb4a2f555076036ec
Han Kim <51018265+hanish3464@users.noreply.github.com>
2020-02-29 00:33:08 +0900

    Update net.py

--- gan/net.py
+++ gan/net.py
@@ -2,33 +2,6 @@ import torch.nn as nn
 import numpy as np
 
 
-# class Generator(nn.Module):
-#     def __init__(self, in_dim=128, shape=28):
-#         super(Generator, self).__init__()
-#         self.in_dim = in_dim
-#         self.shape = shape
-#
-#         def block(in_feat, out_feat, normalize=True):
-#             layers = [nn.Linear(in_feat, out_feat)]
-#             if normalize:
-#                 layers.append(nn.BatchNorm1d(out_feat, 0.8))
-#             layers.append(nn.LeakyReLU(0.2, inplace=True))
-#             return layers
-#
-#         self.model = nn.Sequential(
-#             *block(in_dim, 128, normalize=False),
-#             *block(128, 256),
-#             *block(256, 512),
-#             *block(512, 1024),
-#             nn.Linear(1024, int(np.prod(self.shape))),
-#             nn.Tanh()
-#         )
-#
-#     def forward(self, z):
-#         img = self.model(z)
-#         img = img.view(img.size(0), *self.shape)
-#         return img
-
 class Generator(nn.Module):
     def __init__(self, in_dim=128, shape=28):
         super(Generator, self).__init__()
